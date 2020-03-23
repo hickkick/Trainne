ab3c596eeb823f864ad903351bb2383c9a69908e
Roman Oliinyk <inthewavves@gmail.com>
2020-03-23 20:22:33 +0200

    feat: add Tesla from 0 to 1

---
+++ Tesla/js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $('.slider').slick ({
+        arrows: false,
+        autoplay: 3000,
+        fade: true,
+        dots: true,
+    });
+
+    $('.header-btn').on('click', function() {
+        $('.menu').addClass('active');
+    });
+
+    $('.close-btn').on('click', function() {
+        $('.menu').removeClass('active'); 
+    });
+
+});
